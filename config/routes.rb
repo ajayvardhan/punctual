96ff5dd5806d3366acf088a40271f02fa4e76a49
Prelang Builder <builder@prelang.com>
2014-05-13 08:05:24 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Punctual::Application.routes.draw do
+  get "landings/index"
   resources :events
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
